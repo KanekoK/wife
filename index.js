fc99da58d58465a0a0cc5f90845567059d203d87
kaneko <b1r2d3q4@gmail.com>
2019-01-17 09:23:11 +0900

    .

--- index.js
+++ index.js
@@ -1,100 +1,85 @@
 const http = require('http');
 const https = require('https');
 
+const route = "大江戸線";
+const place = '江東区';
+let reply = "";
+
 function niceDay() {
-  console.log('いってらっしゃい！');
 
   return new Promise((resolve, reject) => {
-    let place = '江東区';
-    let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + encodeURIComponent(place) + '&lang=ja&APPID=eb27703a10fddcb4d47d82eaaebf5cbe';
-    
-    console.log('request:' + url);
-    //httpのリクエストを送信
-    let req = http.get(url, (res) => {
-    let chunk = '';
-    //読み込み中の処理
-    res.on('data', (c) => {
-        chunk += c;
-    });
+    let weather_url = 'http://api.openweathermap.org/data/2.5/weather?q=' + encodeURIComponent(place) + '&lang=ja&APPID=eb27703a10fddcb4d47d82eaaebf5cbe';
+    let train_url = 'https://rti-giken.jp/fhc/api/train_tetsudo/delay.json';
 
-    //読み込み完了時の処理
-    res.on('end', () => {
+    //httpのリクエストを送信
+    let req = http.get(weather_url, (res) => {
+      let chunk = '';
+      //読み込み中の処理
+      res.on('data', (c) => {
+        chunk += c;
+      });
+
+      //読み込み完了時の処理
+      res.on('end', () => {
         let response = JSON.parse(chunk);
         console.log('response: ' + JSON.stringify(response));
-        
+          
         //パラメータの取得
         let weather = response['weather'][0];
         let description = weather['description'];
-        
+          
         //表示
-        console.log(place + 'の天気は' + description + 'です！');
-    
+        reply = 'いってらっしゃい！';
+        reply += place + 'の天気は' + description + 'です！';
+      
         if (description.match(/雨/) || description.match(/雪/)) {
-        console.log('傘を忘れないでね！');
+          reply += '傘を忘れないでね！';
         } else {
-        console.log('今日は傘はいらないよ！');
+          reply += '今日は傘はいらないよ！';
         }
-
-        //処理終了
-        resolve();
+      });
     });
 
-    });
-    //エラー時の処理
-    req.on('error', (e) => {
-      console.error(`エラー： ${e.message}`);
-    });
-  });
-}
-
-function densha() {
-  return new Promise((resolve, reject) => {
-    let url = 'https://rti-giken.jp/fhc/api/train_tetsudo/delay.json';
-    
-    console.log('request:' + url);
     //httpsのリクエストを送信
-    let req = https.get(url, (res) => {
-    let chunk = '';
-    //読み込み中の処理
-    res.on('data', (c) => {
-        chunk += c;
-    });
+    req = https.get(train_url, (res) => {
+      let chunk = '';
+      //読み込み中の処理
+      res.on('data', (c) => {
+          chunk += c;
+      });
 
-    //読み込み完了時の処理
-    res.on('end', () => {
+      //読み込み完了時の処理
+      res.on('end', () => {
         let response = JSON.parse(chunk);
         
         //パラメータの取得
         // console.log(response);
 
-        delay_bool = (function (response) {
+        let delay_bool = (function (response) {
           let result = false;
           response.forEach(function(val, i) {
-            if (val['name'] == "ニューシャトル") {
+            if (val['name'] == route) {
               result = true;
             }
           });
           return result;
         }(response));
+        console.log("delay_bool: " + delay_bool);
 
         if (delay_bool) {
-          console.log("電車の遅延があるので、ご注意ください！");
+          reply += "電車の遅延があるので、ご注意ください！";
         } else {
-          console.log("電車の遅延はありません！");
+          reply += "電車の遅延はありません！";
         }
+        console.log(reply);
 
-
-
-        //処理終了
-        resolve();
-    });
-
-    });
-    //エラー時の処理
-    req.on('error', (e) => {
-      console.error(`エラー： ${e.message}`);
+      //エラー時の処理
+        req.on('error', (e) => {
+          console.error(`エラー： ${e.message}`);
+        });
+      });
     });
   });
 }
 
-densha();
+niceDay();
